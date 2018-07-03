015c8731cda43557883cbc98f90cc8417037e2b3
Sofia Donataccio <srdqty@gmail.com>
2018-07-03 18:13:27 -0400

    Adds minimal json example

--- app/Main.hs
+++ app/Main.hs
@@ -9,23 +9,32 @@ module Main where
 import Data.Text (Text)
 import Network.Wai.Handler.Warp (run)
 import Yesod.Core
-    (RenderRoute (..)
+    ( RenderRoute (..)
     , Yesod
     , mkYesod
     , parseRoutes
     , toWaiApp
+    , requireJsonBody
+    , sendStatusJSON
     )
+import Data.Aeson (Value)
+import Network.HTTP.Types.Status (status201)
 
 data DWWebhooks = DWWebhooks
 
 mkYesod "DWWebhooks" [parseRoutes|
-    / RootR GET
+    / RootR GET POST
 |]
 
 instance Yesod DWWebhooks
 
 getRootR :: Handler Text
 getRootR = pure "Hello, world!"
+
+postRootR :: Handler Text
+postRootR = do
+    v <- requireJsonBody :: Handler Value
+    sendStatusJSON status201 v
 
 main :: IO ()
 main = run 3000 =<< toWaiApp DWWebhooks
