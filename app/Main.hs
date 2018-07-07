1f093746eff26fe3a8e78666cc34840ce677d92f
Sofia Donataccio <srdqty@gmail.com>
2018-07-07 04:22:59 -0400

    Convert to use scotty

--- app/Main.hs
+++ app/Main.hs
@@ -1,73 +1,57 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings     #-}
-{-# LANGUAGE QuasiQuotes           #-}
-{-# LANGUAGE TemplateHaskell       #-}
 {-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Main where
 
 import Control.Monad (unless)
+import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.Reader (runReaderT, lift)
+import Control.Monad.Reader.Class (MonadReader (ask))
 import qualified Data.ByteString as BS (ByteString, drop)
-import qualified Data.ByteString.Lazy as BL (fromChunks, toStrict)
-import Data.Conduit (runConduit, (.|))
-import Data.Conduit.List (consume)
+import qualified Data.ByteString.Lazy as BL (toStrict)
 import Data.Coerce (coerce)
-import Data.Text (Text)
+import qualified Data.Text as T (Text, drop)
+import Data.Text.Encoding (encodeUtf8)
+import qualified Data.Text.Lazy as TL (Text, toStrict)
 import Network.Wai.Handler.Warp (run)
-import Yesod.Core
-    ( RenderRoute (..)
-    , Yesod
-    , mkYesod
-    , parseRoutes
-    , toWaiApp
-    , lookupHeader
-    , sendResponseStatus
-    , notFound
-    , rawRequestBody
-    , MonadHandler
-    )
-import Network.HTTP.Types.Status (status201)
+import Network.HTTP.Types.Status (status201, status404)
 import DWWebhooks.GitHub
+import Web.Scotty.Trans
+    ( ActionT
+    , ScottyError
+    , scottyT
+    , body
+    , get
+    , header
+    , post
+    , status
+    , text
+    )
 
-data DWWebhooks = DWWebhooks
+newtype Config = Config SecretToken
 
-mkYesod "DWWebhooks" [parseRoutes|
-    / RootR GET POST
-|]
+config :: Config
+config = Config (SecretToken "secret")
 
-instance Yesod DWWebhooks
-
-getRootR :: Handler Text
-getRootR = pure "Hello, world!"
-
-requestBody :: MonadHandler m => m BS.ByteString
-requestBody = fmap
-    (BL.toStrict . BL.fromChunks)
-    (runConduit $ rawRequestBody .| consume)
-
-validateGitHubSignature :: BS.ByteString -> Handler ()
-validateGitHubSignature payload = do
-    signature <- getSignature
-    unless (isSignatureValid secret (coerce payload) signature)
-        notFound
+postAction :: (MonadIO m, MonadReader Config m)
+          => ActionT TL.Text m ()
+postAction = do
+    (Config secret) <- lift ask
+    b <- BL.toStrict <$> body
+    maybeSignature <- getSignature
+    isValid <- case maybeSignature of
+        Nothing -> return False
+        Just x -> return (isSignatureValid secret (coerce b) x)
+    status (if isValid then status201 else status404)
     where
-        secret :: SecretToken
-        secret = SecretToken "secret"
-
-        getSignature :: Handler ProvidedSignature
         getSignature = do
-            maybeSignature <- lookupHeader "X-Hub-Signature"
-            case maybeSignature of
-                Nothing -> notFound
-                -- drop the sha1= prefix
-                Just signature -> return (coerce (BS.drop 5 signature))
-
-
-postRootR :: Handler ()
-postRootR = do
-    payload <- requestBody
-    validateGitHubSignature payload
-    sendResponseStatus status201 ()
+            signature <- header "X-Hub-Signature"
+            -- drop the sha1= prefix
+            return (coerce . encodeUtf8 . T.drop 5 . TL.toStrict <$> signature)
 
 main :: IO ()
-main = run 3000 =<< toWaiApp DWWebhooks
+main = scottyT 3000 (`runReaderT` config) $ do
+    get "/" $ text "Hello, world!"
+    post "/" postAction
